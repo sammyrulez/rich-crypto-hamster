444dc2822f9787896f4f618b5a1923ee00253036
sammyrulez <sammyrulez@gmail.com>
2014-03-26 10:44:05 +0100

    refactor

--- exchange/views.py
+++ exchange/views.py
@@ -8,23 +8,21 @@ from event_sourcing import command_executed
 def home(request):
     return render(request, 'index.html')
 
-
-class DepositView(FormView):
-
+class OperationView(FormView):
     template_name = 'operation.html'
     form_class = OperationForm
     success_url = reverse_lazy('home')
+    command_name = None
 
     def form_valid(self, form):
-        command_executed.send(self, command="deposit", payload=form.cleaned_data)
-        return super(DepositView, self).form_valid(form)
+        command_executed.send(self, command=self.command_name, payload=form.cleaned_data)
+        return super(OperationView, self).form_valid(form)
 
-class WithdrawView(FormView):
 
-    template_name = 'operation.html'
-    form_class = OperationForm
-    success_url = reverse_lazy('home')
+class DepositView(OperationView):
+    command_name = 'deposit'
 
-    def form_valid(self, form):
-        command_executed.send(self, command="withdraw", payload=form.cleaned_data)
-        return super(WithdrawView, self).form_valid(form)
+
+
+class WithdrawView(OperationView):
+    command_name = 'withdraw'
