d3d5779d087791f8f448fd10fd91abf21b80958b
sammyrulez <sammyrulez@gmail.com>
2014-04-15 15:01:21 +0200

    base admins views for currancies and ratio

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from exchange.models import Currency, ExchangeRatio
 
-# Register your models here.
+admin.site.register(Currency)
+admin.site.register(ExchangeRatio)
